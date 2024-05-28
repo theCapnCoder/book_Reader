d1993113e8c935aee9bdcd5cab744f42bd7332dd
PavelM <myagkiypavel@gmail.com>
2024-05-28 17:16:32 +0300

    feat: clear project with faker

--- src/app/about/address/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>Address</div>
-  )
-}
-
-export default page
--- src/app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const myName = () => {
-  return <div>About</div>;
-};
-
-export default myName;
