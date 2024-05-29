9b5c5d56a3a929016039ea0dfd3747b664b98d0a
PavelM <myagkiypavel@gmail.com>
2024-05-29 11:56:47 +0300

    test: organization project1

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+const SettingsPage = () => {
+  return (
+    <div>
+      <div>SettingsPage</div>
+      <Button />
+    </div>
+  );
+};
+
+export default SettingsPage;
